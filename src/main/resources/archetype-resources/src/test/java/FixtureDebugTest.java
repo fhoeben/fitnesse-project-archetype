9d91a3680aa3b2b86d244168f485c7ec6ae68f60
Fried Hoeben <github@hsac.nl>
2017-01-28 23:45:34 +0100

    First commit for archetype to create a fitnesse project

---
+++ src/main/resources/archetype-resources/src/test/java/FixtureDebugTest.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import fitnesse.junit.FitNesseRunner;
+import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class to allow fixture code to be debugged, or run by build server.
+ */
+@RunWith(HsacFitNesseRunner.class)
+@FitNesseRunner.Suite("HsacAcceptanceTests.SlimTests.UtilityFixtures.MapFixtureTest")
+public class FixtureDebugTest {
+}
